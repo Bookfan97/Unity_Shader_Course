74bf9305caf5867643e59afa252b70d4ccf4cb12
Noah Day <sheltielover432@gmail.com>
2021-01-26 23:01:39 -0500

    Blending Two Images

---
+++ Shader Course/Assets/Scripts/DecalToggle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DecalToggle : MonoBehaviour
+{
+    Material _material;
+    public bool showDecal = false;
+
+    private void OnMouseDown()
+    {
+        showDecal = !showDecal;
+        if (showDecal)
+        {
+            _material.SetFloat("_ShowDecal", 1);
+        }
+        else
+        {
+            _material.SetFloat("_ShowDecal", 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = this.GetComponent<Renderer>().sharedMaterial;
+        Debug.Log(_material.shader.name.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
